da762c38147ea3954772a7868c02823426339a68
Kyle Kramer <kyle@Kyles-MacBook-Air.local>
2022-03-14 21:19:43 -0500

    Lines

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -8,8 +8,6 @@ let map = L.map("mapid", {
 
 // Get data from cities.js
 let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
@@ -19,6 +17,17 @@ cityData.forEach(function(city) {
   }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
 });
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "red"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
